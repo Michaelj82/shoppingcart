7e1ebd5d37319a17eaa0bd726996da097a3a1e0d
Michael <mjj.yoyogi@gmail.com>
2023-01-24 23:45:09 -0500

    can now add products

--- src/Storecart.js
+++ src/Storecart.js
@@ -1,52 +1,26 @@
 import React, { useContext, useEffect } from "react";
-import { useLoaderData } from "react-router-dom";
 import Context from "./userContext";
-
 function Storecart(props) {
   const {cartData} = useContext(Context)
   
-  // function useData (item){
 
-  //   let name = item['name']
-  //   let price = item['price']
-  //   let quantity = item['quantity']
-
-  //   let itemDiv = document.createElement('div')
-  //   itemDiv.classList.add('cartItem')
-  //   itemDiv.textContent = `name: ${name}, price: ${price}, quantity: ${quantity}`
-
-  //   let newDiv = document.getElementById('cart')
-  //   newDiv.appendChild(itemDiv)
-  // }
-  
 
   useEffect(() => {
-    const newDiv = document.getElementById('cart')
-
-    try{
-      let item = cartData.pop()
-
-      let name = item['name']
-      let price = item['price']
-      let quantity = item['quantity']
-  
-      let itemDiv = document.createElement('div')
-      itemDiv.classList.add('cartItem')
-      itemDiv.textContent = `name: ${name}, price: ${price}, quantity: ${quantity}`
-
-      newDiv.append(itemDiv)
-
-    }catch(TypeError){
-
-    }
     
-
-
   }, [cartData])
 
   return (
     <div id="cart">
       here:
+      {cartData.map(item => (
+        <div className="cartItem">
+          {item.name}{'\n'}
+          Price: {item.price}{'\n'}
+          Quantity: {item.quantity}{'\n'}
+          ------------
+        </div>
+
+      ))}
       </div>
   );
 }
